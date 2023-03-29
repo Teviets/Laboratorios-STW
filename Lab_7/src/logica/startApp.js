ae38be2b9332fd2d2e8a88e7e685b280a38617a7
Teviets <est21405@uvg.edu.gt>
2023-03-28 23:05:11 -0600

    Correcciones

--- Lab_7/src/logica/startApp.js
+++ Lab_7/src/logica/startApp.js
@@ -2,14 +2,13 @@ import { newAudio } from "../modularidad/audios.js";
 import { newEncabezado } from "../modularidad/encabezado.js";
 import { newImg } from "../modularidad/img.js";
 import { newProgress } from "../modularidad/progress.js";
-import { newArticle } from "../modularidad/articulo.js";
 import { newList } from "../modularidad/lista.js";
 import { styleSheet } from "../Modularidad/link.js";
 
 export const indexApp = () => {
     
     document.getElementById('audio').appendChild(newAudio('../assets/sonidos/Bienvenida.wav'));
-    document.getElementById('header').appendChild(newEncabezado('Menu principal', 'Libros populares', 'Nuevos libros', '../DOM/libros-populares.html', '../DOM/nuevos-lanzamientos.html'));
+    document.getElementById('header').appendChild(newEncabezado('Menu principal', 'Libros populares', 'Nuevos libros', './libros-populares.html', './nuevos-lanzamientos.html'));
     document.getElementById('img').appendChild(newImg('../assets/img/Milib.png', 'Mi libreria'));
     document.getElementById('progress').appendChild(newProgress('0'));
     document.head.appendChild(styleSheet('../estilos/index.css'));
---
+++ Lab_7/webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: {
+        index: './src/logica/index.js',
+        librosPop: './src/logica/libros-populares.js',
+        nuevosLanz: './src/logica/nuevos-lanzamientos.js',
+    },
+    output: {
+      filename: '[name].js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html',
+            chunks: ['index']
+        }),
+        new htmlWebpackPlugin({
+            filename: 'libros-populares.html',
+            template: './src/libros-populares.html',
+            chunks: ['librosPop']
+        }),
+        new htmlWebpackPlugin({
+            filename: 'nuevos-lanzamientos.html',
+            template: './src/nuevos-lanzamientos.html',
+            chunks: ['nuevosLanz']
+        }),
+        new MiniCssExtractPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                use: "asset",
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    }
+    
+
+};
+  
